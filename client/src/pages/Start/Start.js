55be8124c96a776122f2f73d3b4f0dccc6b4e589
vanlinh1330 <levanlinh1330@gamil.com>
2023-02-22 01:09:22 +0700

    Ui Start

--- client/src/pages/Start/Start.js
+++ client/src/pages/Start/Start.js
@@ -1,3 +1,8 @@
+import classNames from 'classnames/bind';
+import styles from './Start.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Start() {
     return <h1>Starts page</h1>;
 }
