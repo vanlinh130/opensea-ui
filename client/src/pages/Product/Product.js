41c36b161b69d257a71c3b8c85dd1c325cd2e831
vanlinh1330 <levanlinh1330@gamil.com>
2023-02-24 02:39:06 +0700

    UX Products

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+import { Link, useLocation } from 'react-router-dom';
+import decode from 'jwt-decode';
+
 import styles from './Product.module.scss';
-import { Link } from 'react-router-dom';
 import { LogoIcon } from '~/components/Icons';
 import config from '~/config';
 import images from '~/assets/images';
 import Search from '~/layouts/Components/Header/Search/Search';
 import Button from '~/components/Button/Button';
 import Footer from '~/layouts/Components/Footer/Footer';
+import { Avatar } from '@material-ui/core';
 
 const cx = classNames.bind(styles);
 
 const Product = () => {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const location = useLocation();
+
+    useEffect(() => {
+        const token = user?.token;
+
+        // JWT
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime());
+        }
+
+        setUser(JSON.parse(localStorage.getItem('profile')));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location]);
+
     return (
         <div className={cx('product')}>
             <div className={cx('product-header')}>
@@ -21,22 +41,36 @@ const Product = () => {
                         <LogoIcon className={cx('brand-name')} />
                         <h3>Cart</h3>
                     </Link>
-                    <Link to="" className={cx('nav-avatar')}>
+                    <div className={cx('nav-avatar')}>
                         <Search placeholder="Find products..." />
-                        <img src={images.detail_avatar} alt="images" />
-                        <h5>Văn linh</h5>
-                    </Link>
+                        {user ? (
+                            <Link to={config.routes.profile} className={cx('img-avatar-no')}>
+                                <Avatar src={user.result.imageUrl} alt={user.result.name} className={cx('img-avatar')}>
+                                    {user.result.name.charAt(0)}
+                                </Avatar>
+                                <h5>{user.result.name}</h5>
+                            </Link>
+                        ) : (
+                            <Link to={config.routes.auth} className={cx('img-avatar-no')}>
+                                <Avatar src={images.no_avatar} alt="images"></Avatar>
+                                <h5>Login</h5>
+                            </Link>
+                        )}
+                    </div>
                 </div>
             </div>
             <div className={cx('product-content')}>
-                <div className={cx('product-buy')}></div>
-                <div className={cx('product-no-buy')}>
-                    <img src={images.product_cart_no} alt="images-no-buy" />
-                    <p>Giỏ hàng của bạn còn trống</p>
-                    <Button outGreen rounded>
-                        MUA NGAY
-                    </Button>
-                </div>
+                {user ? (
+                    <div className={cx('product-buy')}></div>
+                ) : (
+                    <div className={cx('product-no-buy')}>
+                        <img src={images.product_cart_no} alt="images-no-buy" />
+                        <p>Giỏ hàng của bạn còn trống</p>
+                        <Button outGreen rounded>
+                            MUA NGAY
+                        </Button>
+                    </div>
+                )}
             </div>
 
             <Footer />
