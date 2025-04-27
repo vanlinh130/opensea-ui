2a11f94c9dc872a988b35f5889215fa09d632887
vanlinh130 <linhvanle130@gmail.com>
2025-04-27 19:09:40 +0700

    fix env mongodb false

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ app.use('/user', userRoutes);
 const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
+mongoose.set('strictQuery', false);
+
 mongoose
     .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
